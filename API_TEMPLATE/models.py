a86859e1e552624e81c97c797a7578a97ce0049f
billy <billy006jkr@gmaik.com>
2026-08-09 16:27:23 -0500

    update requeritments and models

--- API_TEMPLATE/models.py
+++ API_TEMPLATE/models.py
@@ -48,3 +48,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.instrument}"
+    
+class Album(models.Model):
+    musician = models.ForeignKey
+    
